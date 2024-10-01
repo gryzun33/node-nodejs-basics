f116e140968b24043138c899bf1e6942fe81edb1
gryzun33 <yakushevao777@gmail.com>
2024-10-01 14:21:38 +0300

    feat: add task copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToOldDir = path.join(__dirname, 'files');
+  const pathToNewDir = path.join(__dirname, 'files-copy');
+  try {
+    await fs.access(pathToOldDir, fs.constants.F_OK);
+    await fs.mkdir(pathToNewDir);
+    const files = await fs.readdir(pathToOldDir);
+    for (const file of files) {
+      const pathToOldFile = path.join(pathToOldDir, file);
+      const pathToNewFile = path.join(pathToNewDir, file);
+      await fs.copyFile(pathToOldFile, pathToNewFile);
+    }
+  } catch (err) {
+    if (err.code === 'EEXIST' || err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      console.error(err.message);
+    }
+  }
 };
 
 await copy();
