7300cf95fb2000ad4529183e1b7cc8ccb059cf1c
gryzun33 <yakushevao777@gmail.com>
2024-10-01 13:28:00 +0300

    feat: add task create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+  const text = 'I am fresh and young';
+  try {
+    await fs.access(pathToFile, fs.constants.F_OK);
+    throw new Error();
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await fs.writeFile(pathToFile, text, 'utf8');
+    } else {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await create();
