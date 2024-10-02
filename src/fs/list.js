1115f65c36788e68bdf280418bb56b507a9f2435
gryzun33 <yakushevao777@gmail.com>
2024-10-02 12:23:28 +0300

    feat: add task list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToDir = path.join(__dirname, 'files');
+
+  try {
+    await fs.access(pathToDir);
+    const files = await fs.readdir(pathToDir);
+    console.log(files);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      console.error(err.message);
+    }
+  }
 };
 
 await list();
