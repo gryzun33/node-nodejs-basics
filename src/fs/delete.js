7e4db7278ae5e1aa9694ec19572cb8b4a290adb0
gryzun33 <yakushevao777@gmail.com>
2024-10-02 12:12:27 +0300

    feat: add task delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const remove = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToFile = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await fs.rm(pathToFile);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      console.error(err.message);
+    }
+  }
 };
 
 await remove();
