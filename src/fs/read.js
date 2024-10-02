e7a1ddfd86bef75d91cd83f32016961d7aaa4fcf
gryzun33 <yakushevao777@gmail.com>
2024-10-02 12:33:01 +0300

    feat: add task read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+  try {
+    const text = await fs.readFile(pathToFile, 'utf8');
+    console.log(text);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      console.error(err.message);
+    }
+  }
 };
 
 await read();
