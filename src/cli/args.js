219a05fff6ba73c272851eab30320b592b180cc7
gryzun33 <yakushevao777@gmail.com>
2024-10-02 13:40:49 +0300

    feat: add task args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const arrArgs = argv.slice(2);
+  const resultArr = [];
+  for (let i = 0; i < arrArgs.length - 1; i += 2) {
+    const str = arrArgs[i].slice(2) + ' is ' + arrArgs[i + 1];
+    resultArr.push(str);
+  }
+  const result = resultArr.join(', ');
+  console.log(result);
 };
 
 parseArgs();
