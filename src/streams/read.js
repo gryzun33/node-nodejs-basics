92d06561fd73b2f88c7bb4ee5a1a72cb862e04eb
gryzun33 <yakushevao777@gmail.com>
2024-10-03 16:04:45 +0300

    feat: add task streams-read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import * as url from 'url';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToFile = path.join(__dirname, './files/fileToRead.txt');
+  const stream = createReadStream(pathToFile);
+
+  let data = '';
+
+  stream.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  stream.on('end', () => {
+    process.stdout.write(data + '\n');
+  });
 };
 
 await read();
