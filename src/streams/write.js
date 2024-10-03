406950d0a1fd896a93b763a3e7c55f091d89d553
gryzun33 <yakushevao777@gmail.com>
2024-10-03 16:39:22 +0300

    feat: add task streams-write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import * as url from 'url';
+const { stdin, stdout } = process;
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToFile = path.join(__dirname, './files/fileToWrite.txt');
+  const stream = createWriteStream(pathToFile);
+
+  stdin.on('data', (data) => {
+    stream.write(data);
+  });
 };
 
 await write();
