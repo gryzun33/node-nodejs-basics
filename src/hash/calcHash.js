f566fb47ffaa94e11eae5b81345699a6e40b05fa
gryzun33 <yakushevao777@gmail.com>
2024-10-03 15:45:24 +0300

    feat: add task calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+import * as url from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToFile = path.join(__dirname, './files/fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const stream = createReadStream(pathToFile);
+
+  stream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  stream.on('end', () => {
+    const result = hash.digest('hex');
+    process.stdout.write(result + '\n');
+  });
 };
 
 await calculateHash();
