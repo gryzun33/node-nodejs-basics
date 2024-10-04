7562374c08c60185596b431e09fb59250dd7ff72
gryzun33 <yakushevao777@gmail.com>
2024-10-04 12:36:46 +0300

    feat: add task zip-decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream';
+import path from 'path';
+import * as url from 'url';
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const pathToZipFile = path.join(__dirname, 'files', 'archive.gz');
+  const readable = createReadStream(pathToZipFile);
+  const writeable = createWriteStream(pathToFile);
+  const gunzip = createGunzip();
+  pipeline(readable, gunzip, writeable, (err) => {
+    if (err) {
+      console.error(`Error: ${err.message}`);
+    }
+  });
 };
 
 await decompress();
