f0352821ac18179c594d078c577ed28b93099288
gryzun33 <yakushevao777@gmail.com>
2024-10-04 11:02:51 +0300

    feat: change write.js with pipe method

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,16 +1,13 @@
-import { createWriteStream } from 'fs';
+import { createWriteStream, read } from 'fs';
 import path from 'path';
 import * as url from 'url';
-const { stdin, stdout } = process;
 
 const write = async () => {
   const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
   const pathToFile = path.join(__dirname, './files/fileToWrite.txt');
-  const stream = createWriteStream(pathToFile);
-
-  stdin.on('data', (data) => {
-    stream.write(data);
-  });
+  const writable = createWriteStream(pathToFile);
+  const readable = process.stdin;
+  readable.pipe(writable);
 };
 
 await write();
