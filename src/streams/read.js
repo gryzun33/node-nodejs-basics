b73f5ffd191332193391fa405712dcacf45861f7
gryzun33 <yakushevao777@gmail.com>
2024-10-04 10:57:44 +0300

    feat: change read.js with pipe method

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,20 +1,16 @@
 import { createReadStream } from 'fs';
 import path from 'path';
 import * as url from 'url';
+import os from 'os';
 
 const read = async () => {
   const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
   const pathToFile = path.join(__dirname, './files/fileToRead.txt');
   const stream = createReadStream(pathToFile);
 
-  let data = '';
-
-  stream.on('data', (chunk) => {
-    data += chunk;
-  });
-
+  stream.pipe(process.stdout);
   stream.on('end', () => {
-    process.stdout.write(data + '\n');
+    process.stdout.write(`${os.EOL}`);
   });
 };
 
