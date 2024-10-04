d258014d4a92731330d3d35aafeb4ee352f48e60
gryzun33 <yakushevao777@gmail.com>
2024-10-04 16:44:56 +0300

    feat: add task cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,29 @@
+import { fork } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  const child = fork(pathToFile, args, {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+  });
+
+  process.stdin.on('data', (data) => {
+    child.stdin.write(data);
+  });
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`Error: ${data}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 'a']);
