ffebae22a531a9ad5ff6c4f3cc59f699d03415a6
gryzun33 <yakushevao777@gmail.com>
2024-10-04 12:02:43 +0300

    feat: add task zlib-compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import path from 'path';
+import * as url from 'url';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const pathToZipFile = path.join(__dirname, 'files', 'archive.gz');
+  const readable = createReadStream(pathToFile);
+  const writeable = createWriteStream(pathToZipFile);
+  const gzip = createGzip();
+  pipeline(readable, gzip, writeable, (err) => {
+    if (err) {
+      console.error(`Error: ${err.message}`);
+    }
+  });
 };
 
 await compress();
