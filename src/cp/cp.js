c41c38d0b357f2dba33dd77f516666bffe07f4b9
gryzun33 <yakushevao777@gmail.com>
2024-10-07 17:16:08 +0300

    fix: fix task cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -7,21 +7,7 @@ const spawnChildProcess = async (args) => {
   const __dirname = path.dirname(__filename);
   const pathToFile = path.join(__dirname, 'files', 'script.js');
 
-  const child = fork(pathToFile, args, {
-    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
-  });
-
-  process.stdin.on('data', (data) => {
-    child.stdin.write(data);
-  });
-
-  child.stdout.on('data', (data) => {
-    process.stdout.write(data);
-  });
-
-  child.stderr.on('data', (data) => {
-    console.error(`Error: ${data}`);
-  });
+  const child = fork(pathToFile, args);
 };
 
 // Put your arguments in function call to test this functionality
